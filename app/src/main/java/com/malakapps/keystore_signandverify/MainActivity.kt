85ef47463e319713806bc0dfafcdb7b1c2fece48
Abel Suviri Payán <asuviri@ieDigital.com>
2019-03-23 21:48:16 +0000

    Add comments and method to check if keys already exists

--- app/src/main/java/com/malakapps/keystore_signandverify/MainActivity.kt
+++ app/src/main/java/com/malakapps/keystore_signandverify/MainActivity.kt
@@ -30,11 +30,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        //Check if lock screen has been set up. Just displaying a Toast here but it shouldn't allow the user to go forward.
         if (!keyguardManager.isDeviceSecure) {
             Toast.makeText(this, "Secure lock screen hasn't set up.", Toast.LENGTH_LONG).show()
         }
 
-        generateKey()
+        //Check if the keys already exists to avoid creating them again
+        if (!checkKeyExists()) {
+            generateKey()
+        }
 
         signButton.setOnClickListener {
             signData()
@@ -46,36 +51,59 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun generateKey() {
+        //We create the start and expiry date for the key
         val startDate = GregorianCalendar()
         val endDate = GregorianCalendar()
         endDate.add(Calendar.YEAR, 1)
 
+        //We are creating a RSA key pair and store it in the Android Keystore
         val keyPairGenerator: KeyPairGenerator = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, ANDROID_KEYSTORE)
 
-        val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(KEY_ALIAS, KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY).run {
-            setCertificateSerialNumber(BigInteger.valueOf(777))
-            setCertificateSubject(X500Principal("CN=$KEY_ALIAS"))
-            setDigests(KeyProperties.DIGEST_SHA256)
-            setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
-            setCertificateNotBefore(startDate.time)
-            setCertificateNotAfter(endDate.time)
-            setUserAuthenticationRequired(true)
-            setUserAuthenticationValidityDurationSeconds(30)
+        //We are creating the key pair with sign and verify purposes
+        val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(KEY_ALIAS,
+            KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY).run {
+            setCertificateSerialNumber(BigInteger.valueOf(777))       //Serial number used for the self-signed certificate of the generated key pair, default is 1
+            setCertificateSubject(X500Principal("CN=$KEY_ALIAS"))     //Subject used for the self-signed certificate of the generated key pair, default is CN=fake
+            setDigests(KeyProperties.DIGEST_SHA256)                         //Set of digests algorithms with which the key can be used
+            setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1) //Set of padding schemes with which the key can be used when signing/verifying
+            setCertificateNotBefore(startDate.time)                         //Start of the validity period for the self-signed certificate of the generated, default Jan 1 1970
+            setCertificateNotAfter(endDate.time)                            //End of the validity period for the self-signed certificate of the generated key, default Jan 1 2048
+            setUserAuthenticationRequired(true)                             //Sets whether this key is authorized to be used only if the user has been authenticated, default false
+            setUserAuthenticationValidityDurationSeconds(30)                //Duration(seconds) for which this key is authorized to be used after the user is successfully authenticated
             build()
         }
 
+        //Initialization of key generator with the parameters we have specified above
         keyPairGenerator.initialize(parameterSpec)
 
+        //Generates the key pair
         keyPair = keyPairGenerator.genKeyPair()
+    }
+
+    private fun checkKeyExists(): Boolean {
+        //We get the Keystore instance
+        val keyStore: KeyStore = KeyStore.getInstance(ANDROID_KEYSTORE).apply {
+            load(null)
+        }
+
+        //We get the private and public key from the keystore if they exists
+        val privateKey: PrivateKey? = keyStore.getKey(KEY_ALIAS, null) as PrivateKey?
+        val publicKey: PublicKey? = keyStore.getCertificate(KEY_ALIAS)?.publicKey
+
+        return privateKey != null && publicKey != null
     }
 
     private fun signData() {
         try {
+            //We get the Keystore instance
             val keyStore: KeyStore = KeyStore.getInstance(ANDROID_KEYSTORE).apply {
                 load(null)
             }
 
+            //Retrieves the private key from the keystore
             val privateKey: PrivateKey = keyStore.getKey(KEY_ALIAS, null) as PrivateKey
+
+            //We sign the data with the private key. We use RSA algorithm along SHA-256 digest algorithm
             val signature: ByteArray? = Signature.getInstance("SHA256withRSA").run {
                 initSign(privateKey)
                 update("TestString".toByteArray())
@@ -83,15 +111,16 @@ class MainActivity : AppCompatActivity() {
             }
 
             if (signature != null) {
+                //We encode and store in a variable the value of the signature
                 signatureResult = Base64.encodeToString(signature, Base64.DEFAULT)
                 resultTextView.text = "Signed successfully"
             }
 
         } catch (e: UserNotAuthenticatedException) {
-            showAuthenticationScreen()
-        } catch(e: SignatureException) {
+            //Exception thrown when the user has not been authenticated.
             showAuthenticationScreen()
         } catch (e: KeyPermanentlyInvalidatedException) {
+            //Exception thrown when the key has been invalidated for example when lock screen has been disabled.
             Toast.makeText(this, "Keys are invalidated.\n" + e.message, Toast.LENGTH_LONG).show()
         } catch (e: Exception) {
             throw RuntimeException(e)
@@ -99,15 +128,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun verifyData() {
+        //We get the Keystore instance
         val keyStore: KeyStore = KeyStore.getInstance(ANDROID_KEYSTORE).apply {
             load(null)
         }
 
-        //If we need the public key just certificate.publicKey
+        //We get the certificate from the keystore
         val certificate: Certificate? = keyStore.getCertificate(KEY_ALIAS)
 
         if (certificate != null) {
+            //We decode the signature value
             val signature: ByteArray = Base64.decode(signatureResult, Base64.DEFAULT)
+
+            //We check if the signature is valid. We use RSA algorithm along SHA-256 digest algorithm
             val isValid: Boolean = Signature.getInstance("SHA256withRSA").run {
                 initVerify(certificate)
                 update("TestString".toByteArray())
@@ -123,7 +156,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showAuthenticationScreen() {
-        val intent: Intent? = keyguardManager.createConfirmDeviceCredentialIntent(null, null)
+        //This will open a screen to enter the user credentials (fingerprint, pin, pattern). We can display a custom title and description
+        val intent: Intent? = keyguardManager.createConfirmDeviceCredentialIntent("Keystore Sign And Verify",
+            "To be able to sign the data we need to confirm your identity. Please enter your pin/pattern or scan your fingerprint")
         if (intent != null) {
             startActivityForResult(intent, REQUEST_CODE_FOR_CREDENTIALS)
         }
@@ -134,7 +169,7 @@ class MainActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK) {
                 signData()
             } else {
-                Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
             }
         }
     }
